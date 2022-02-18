9789826b5df6f71d5843132b5d50c82b40058964
Ben Cronheim <bc06026@uga.edu>
2022-02-18 17:06:19 -0500

    Update app views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Pokemon
+from .serializers import PokemonSerializer
 
 # Create your views here.
+
+class PokemonViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Pokemon.objects.all()
+    serializer_class = PokemonSerializer
+    def get_queryset(self):
+        queryset = Pokemon.objects.all()
+        name = self.request.query_params.get('name')
+        if name is not None:
+            queryset = queryset.filter(name=name)
+        return queryset
